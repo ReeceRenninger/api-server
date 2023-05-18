7b23507b7d22295c2fa75eb173b1e78d85a426b9
ReeceRenninger <renningerreece@gmail.com>
2023-05-17 17:38:28 -0700

    partial routes working

--- src/routes/food.js
+++ src/routes/food.js
@@ -2,16 +2,48 @@
 
 const express = require('express');
 const router = express.Router();
-const { foodModel } = require('../models/food');
+const { foodModel } = require('../models/index');
 
-router.get('/food', async (req, res, next) => {
-  res.status(200).send('food route is alive!');
-  let singleFoodItem = await foodModel.findAll({where: {id: req.params.id}});
 
-  res.status(200).send(singleFoodItem);
-});
-router.post('/food', async (req, res, next) => {
+//Create a record //!! WORKING DO NOT TOUCH
+router.post('/food', async (req, res) => {
+  console.log('this is the req body from post function', req.body);
   let newFood = await foodModel.create(req.body);
 
   res.status(200).send(newFood);
 });
+
+//Get one record //!! WORKING DO NOT TOUCH
+router.get('/food/:id', async (req, res) => {
+  let singleFoodItem = await foodModel.findAll({ where: {id: req.params.id }}); // find entry by primary key provided
+
+  if (singleFoodItem === null) {
+    console.log('Food item not found!');
+  } else {
+    res.status(200).send(singleFoodItem);
+  }
+});
+
+//Get all records //!! WORKING DO NOT TOUCH
+router.get('/food', async (req, res) => {
+  let allFoodItems = await foodModel.findAll();
+  console.log(allFoodItems);
+  res.status(200).send(allFoodItems);
+});
+
+//Update a record
+// router.put()
+
+//Delete is working but throwing an error and crashing server
+router.delete('/food/:id', async (req, res) => {
+  let id = parseInt(req.params.id);
+  let deleteOneFoodItem = await foodModel.destroy({
+    where: {
+      id,
+    },
+  });
+
+  res.status(200).send(deleteOneFoodItem);
+});
+
+module.exports = router;
--- src/routes/ingredients.js
+++ src/routes/ingredients.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const express = require('express')
+const router = express.Router();
+const { ingredientsModel } = require('../models/ingredients');
+
+
+router.post('/ingredients', async (req, res, next) => {
+  console.log('this is the req body from ingredients post:', req.body);
+  let newIngredient = await ingredientsModel.create(req.body);
+
+  res.status(200).send(newIngredient);
+});
+
+
+router.get('/ingredients', (req, res, next) => {
+
+  res.status(200).send('ingredients route is alive!');
+});
+
+
+
+
+module.exports = router;
