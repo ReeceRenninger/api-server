b14b6b4e2a4c588640fdef6b59434ba89e985851
ReeceRenninger <renningerreece@gmail.com>
2023-05-21 10:55:47 -0700

    got test suite working finally, but tests are not passing. at least registering now though

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,7 +3,6 @@
 const supertest = require('supertest');
 const { app } = require('../src/server');
 const { sequelizeDatabase } = require('../src/models');
-const { test } = require('node:test');
 const mockRequest = supertest(app);
 
 const request = supertest(app);
@@ -11,7 +10,6 @@ const request = supertest(app);
 beforeAll(async () => {
   await sequelizeDatabase.sync();
 });
-
 
 //NUKES THE TEST TO END
 afterAll( async () =>{
@@ -22,31 +20,42 @@ describe('Server', () => {
   //test for proof of life path
   test('handles root path', async () => {
     const response = await mockRequest.get('/');
-
+    
     expect(response.status).toBe(200);
     expect(response.text).toEqual('I AM ALIVE!');
   });
-
-  test('handles food router', async () => {
-    const response = await request.get('/food');
+  
+  test('describes food router', async () => {
+    const response = (await request.post('/food')).send({
+      name: 'Test',
+      category: 'fruit',
+    });
 
     expect(response.status).toEqual(200);
-    expect(response.body.name).toEqual('test');
-    expect(response.body.category).toEqual('test');
+    expect(response.body.name).toEqual('Test');
+    expect(response.body.category).toEqual('fruit');
+    
+   
   });
-
+  
   test('handle ingredient router', async () => {
-    const response = await request.get('/ingredients');
-
+    const response = await request.post('/ingredients').send({
+      name: 'Test',
+      quantity: 1,
+      foodId: 1,
+    });
+    
     expect(response.status).toEqual(200);
-    expect(response.body.name).toEqual('test');
-    expect(response.body.category).toEqual('test');
+    expect(response.body.name).toEqual('Test');
+    expect(response.body.quantity).toEqual(1);
+    expect(response.body.foodId).toEqual(1);
   });
-
+  
   //test for non existent route
   test('handle not found', async () => {
     const response = await mockRequest.get('/foo');
     expect(response.status).toEqual(404);
   });
-
+  
 });
+
